0989a214d6771d7f2766b0bf01a84c593159d374
DariaVorobyova <dasha090498@gmail.com>
2025-07-02 22:30:03 -0300

    small improvement with typization for wave text animation

--- src/components/ui/WaveText.vue
+++ src/components/ui/WaveText.vue
@@ -1,58 +1,55 @@
 <template>
-    <span class="wave-text">
-      <span
-        v-for="(char, i) in splitText"
-        :key="i"
-        class="char"
-      >
-        {{ char === ' ' ? '\u00A0' : char }}
-      </span>
+  <span class="wave-text" ref="textRef">
+    <span v-for="(char, i) in splitText" :key="i" class="char">
+      {{ char === " " ? "\u00A0" : char }}
     </span>
-  </template>
-  
-  <script>
-  import gsap from 'gsap';
-  
-  export default {
-    name: 'WaveText',
-    props: {
-      text: {
-        type: String,
-        required: true,
-      },
+  </span>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, onMounted } from "vue";
+import gsap from "gsap";
+
+interface Props {
+  text: string;
+}
+
+const props = defineProps<Props>();
+
+const textRef = ref<HTMLElement | null>(null);
+
+const splitText = computed(() => props.text.split(""));
+
+onMounted(() => {
+  if (!textRef.value) return;
+
+  const chars = textRef.value.querySelectorAll<HTMLElement>(".char");
+
+  gsap.to(chars, {
+    y: -10,
+    ease: "sine.inOut",
+    duration: 0.6,
+    repeat: -1,
+    yoyo: true,
+    stagger: {
+      each: 0.05,
+      from: "center",
     },
-    computed: {
-      splitText() {
-        return this.text.split('');
-      },
-    },
-    mounted() {
-      gsap.to(this.$el.querySelectorAll('.char'), {
-        y: -10,
-        ease: 'sine.inOut',
-        duration: 0.6,
-        repeat: -1,
-        yoyo: true,
-        stagger: {
-          each: 0.05,
-          from: 'center',
-        },
-      });
-    },
-  };
-  </script>
-  
-  <style scoped lang="scss">
-  .wave-text {
-    display: inline-block;
-  }
-  
-  .char {
-    display: inline-block;
-    background-image: linear-gradient($secondary, #ffffff);
-    background-clip: text;
-    -webkit-background-clip: text;
-    color: transparent;
-    will-change: transform;
-  }
-  </style>
+  });
+});
+</script>
+
+<style scoped lang="scss">
+.wave-text {
+  display: inline-block;
+}
+
+.char {
+  display: inline-block;
+  background-image: linear-gradient($secondary, #ffffff);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  will-change: transform;
+}
+</style>
